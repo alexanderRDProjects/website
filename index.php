11e994d3e99b9ac0d504367684c6d6f8a048ee68
alexanderRDProjects <a.rdunn.yw@gmail.com>
2016-05-30 14:01:32 +0100

    added basic styled navigation bar and information

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php 
-echo "Hello World"
-?>
+<html>
+<head>
+<title>Alexander Projects</title>
+</head>
+<body>
+<div>
+<ul><li><a>home</a></li><li><a>BRBS</a></li><li><a>Riddler</a></li><li><a>Other Projects</a></li></ul>
+</div>
+<p>Hi i am a student that has devolped some small scale solution my main foucs is in PHP web-design however I have some other project some as a high level natural programming language called PsuedoSense</p>
+</body>
+</html>
+
